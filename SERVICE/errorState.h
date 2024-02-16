29511a0c9958e0a02e8692c0da5931bc73b14468
raghad islam <raghad.noser.2003@gmail.com>
2024-02-16 13:26:31 +0200

    update errors states

--- SERVICE/errorState.h
+++ SERVICE/errorState.h
@@ -8,7 +8,7 @@
 #ifndef ERRORSTATE_H_
 #define ERRORSTATE_H_
 
-
+/* enum to define all error types needed */
 typedef enum{
 	ES_NOK,
 	ES_OK,
@@ -18,8 +18,16 @@ typedef enum{
 	ES_PIN_NOT_CONNECTED,
 	ES_WEONG_PIN,
 	ES_UNSUPPORTED_MODE,
+	ES_TIME_OUT,
 	ES_BUSY_STATE,
-	ES_TIME_OUT
+	ES_RESERVED_ADDRESSES,
+	ES_START_COND,
+	ES_REP_START_COND,
+	ES_SLAVE_ADD_WITH_WR,
+	ES_SLAVE_ADD_WITH_RD,
+	ES_MASTER_WR_BYTE,
+	ES_MASTER_RD_BYTE
+
 }ES_t;
 
 #endif /* ERRORSTATE_H_ */
